ae4e6a85b445260f3fac7dc89f0342f6f6931504
Garry Work <gtoborn@gmail.com>
2017-09-01 16:07:00 -0600

    new additions

--- practice.js
+++ practice.js
@@ -3,10 +3,10 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+//Code here
 var me = {
-  name: 'Garry',
-  age: 44
+    name: 'Garry',
+    age: 44
 }
 alert(me.name);
 
@@ -20,14 +20,14 @@ alert(me.name);
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+//Code here
 var favoriteThings = {
-  band: 'Rush',
-  food: 'Italian',
-  person: 'Messi',
-  book: 'Lord of the Rings',
-  movie: 'Heat',
-  holiday: 'Fall Season',
+    band: 'Rush',
+    food: 'Italian',
+    person: 'Messi',
+    book: 'Lord of the Rings',
+    movie: 'Heat',
+    holiday: 'Fall Season',
 }
 
 
@@ -37,7 +37,7 @@ var favoriteThings = {
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
+//Code here
 favoriteThings.car = 'Toyota';
 favoriteThings.brand = 'Tacoma';
 
@@ -46,7 +46,7 @@ favoriteThings.brand = 'Tacoma';
 
 //Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 
-  //Code here
+//Code here
 favoriteThings.food = 'Chicken Nuggets';
 favoriteThings.book = 'Harry Potter';
 
@@ -67,9 +67,8 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. 
 */
 
-  //Code here
-var backPack = {
-}
+//Code here
+var backPack = {}
 
 var item = 'firstPocket';
 
@@ -79,9 +78,9 @@ backPack.color = 'black';
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
+//Code here
 /*alert(backPack); */
- 
+
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -89,7 +88,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-  //Code here
+//Code here
 console.log(backPack);
 
 
@@ -101,17 +100,17 @@ console.log(backPack);
 
 
 var user2 = {
-        name: 'Ty',
-        age: 24,
-        pwHash: 'U+Ldlngx2BYQk',
-        email: 'ty33@gmail.com',
-        birthday: '05/02/1990',
-        username: 'tylermcginnis33'
+    name: 'Ty',
+    age: 24,
+    pwHash: 'U+Ldlngx2BYQk',
+    email: 'ty33@gmail.com',
+    birthday: '05/02/1990',
+    username: 'tylermcginnis33'
 };
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
-  //Code Here
+//Code Here
 user2.name = 'Tyler S. McGinnis';
 user2.email = 'tyler.mcginnis@devmounta.in';
 
@@ -134,8 +133,10 @@ user2.email = 'tyler.mcginnis@devmounta.in';
 
 //Create an empty object called methodCollection.
 
-  //Code Here
+//Code Here
+var methodCollection = {
 
+}
 
 
 
@@ -145,17 +146,22 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
  which logs 'hello' to the console. 
  */
 
-  //Code Here
+//Code Here
+methodCollection.alertHello = function() {
+    alert('hello');
+}
 
-
+methodCollection.logHello = function() {
+    console.log('hello');
+}
 
 
 
 //Now call your alertHello and logHello methods.
 
-  //Code Here
-
-
+//Code Here
+methodCollection.alertHello();
+methodCollection.logHello();
 
 
 
@@ -169,13 +175,18 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 // Create a function called makePerson which takes in name, birthday, ssn as its
 // parameters and returns a new object with all of the information that you passed in.
 
-  //Code Here
+//Code Here
+function makePerson(name, birthday, ssn) {
+    this.name = name;
+    this.birthday = birthday;
+    this.ssn = ssn;
+}
 
 
 
 
 
-  
+
 
 
 //NEXT PROBLEM
@@ -184,6 +195,4 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
 // Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object and returns that object so that whenever you invoke makeCard, you get a brand new credit card.
 
-  //Code Here
-
-
+//Code Here
